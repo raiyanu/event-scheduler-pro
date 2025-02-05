48c1532f1f5e2f2ecfcc48209a2a37f99e0160bc
Raiyan Ahmed <ray38250968@gmail.com>
2025-02-05 16:31:33 +0530

    Chore: adjusted calender

--- src/app/calendar/page.jsx
+++ src/app/calendar/page.jsx
@@ -1,13 +1,18 @@
 "use client"
 import React, { useState } from 'react'
 import MyCalendar from '../component/MyCalendar'
+import MainLayout from '../PrimaryLayout'
+import { Box, Typography } from '@mui/material'
+import { ErrorBoundary } from 'next/dist/client/components/error-boundary'
 
 export default function Calendar() {
-    const [count, setCount] = useState(0)
     return (
-        <div className='h-screen p-8'>
-            <MyCalendar />
-            <button className='btn' onClick={() => setCount(count + 1)}>{count}</button>
-        </div>
+        <MainLayout>
+            <Box className="h-full p-2 lg:p-3">
+                {/* <ErrorBoundary errorComponent={<Typography>Error</Typography>} errorScripts={() => console.log("Errror occured")}> */}
+                <MyCalendar />
+                {/* </ErrorBoundary> */}
+            </Box>
+        </MainLayout>
     )
 }
